1bc8bc7a1d2e2beeffcfd1919da367e73eabae66
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2023-11-05 22:03:28 +0800

    remove dev options

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,6 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
       manifest: {
         icons: [
           {
